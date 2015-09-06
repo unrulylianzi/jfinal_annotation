6dfc6af79eca824c1e50bef6be2f8b552ef31fe1
明笃 <lianzi_uestc@163.com>
2015-09-06 23:44:07 +0800

    注释版jfinal

--- src/com/jfinal/core/JFinalFilter.java
+++ src/com/jfinal/core/JFinalFilter.java
@@ -25,6 +25,8 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+
 import com.jfinal.config.Constants;
 import com.jfinal.config.JFinalConfig;
 import com.jfinal.handler.Handler;
@@ -42,22 +44,33 @@ public final class JFinalFilter implements Filter {
 	private static final JFinal jfinal = JFinal.me();
 	private static Logger log;
 	private int contextPathLength;
-	
+
+	/**
+	 * 实现init方法，通过servlet规范配置filter的属性，configClass，这就是整个项目的配置文件，定义一些配置，TODO,可以实现分布式的配置。
+	 * jetty容器启动的时候会根据web.xml的配置，调用init()方法，加载信息只会执行一次。
+	 * */
+	@Override
 	public void init(FilterConfig filterConfig) throws ServletException {
+		//创建JFinalConfig对象
 		createJFinalConfig(filterConfig.getInitParameter("configClass"));
-		
+
+		//初始化插件和其他资源
 		if (jfinal.init(jfinalConfig, filterConfig.getServletContext()) == false)
 			throw new RuntimeException("JFinal init error!");
 		
 		handler = jfinal.getHandler();
 		constants = Config.getConstants();
 		encoding = constants.getEncoding();
+		//系统启动之后做的相关工作,如果需要实现，可以实现相应的回调函数
 		jfinalConfig.afterJFinalStart();
-		
+
+		//contextPath的值 TODO
 		String contextPath = filterConfig.getServletContext().getContextPath();
 		contextPathLength = (contextPath == null || "/".equals(contextPath) ? 0 : contextPath.length());
 	}
-	
+	/**
+	 * 实现dofilter方法，
+	 * */
 	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
 		HttpServletRequest request = (HttpServletRequest)req;
 		HttpServletResponse response = (HttpServletResponse)res;
@@ -66,9 +79,11 @@ public final class JFinalFilter implements Filter {
 		String target = request.getRequestURI();
 		if (contextPathLength != 0)
 			target = target.substring(contextPathLength);
-		
+		//Handler拥有web请求的绝对控制权，一个handler chain在配置类里面配置好，会按照这个chain根据handler的逻辑，决定是否走下去
+		//数组传值，关于传值和引用向来有争议
 		boolean[] isHandled = {false};
 		try {
+			//责任链模式
 			handler.handle(target, request, response, isHandled);
 		}
 		catch (Exception e) {
@@ -77,22 +92,43 @@ public final class JFinalFilter implements Filter {
 				log.error(qs == null ? target : target + "?" + qs, e);
 			}
 		}
-		
+		//如果isHandled[0]为假则继续
 		if (isHandled[0] == false)
 			chain.doFilter(request, response);
 	}
-	
+
+	/**
+	 * 实现destroy方法，容器关闭时调用destroy()方法进行销毁。
+	 * JFinalConfig 中的 afterJFinalStart()与 beforeJFinalStop()方法供开发者在 JFinalConfig 继承类中 覆盖。reference:
+	 * jfinal-2.0-manual.pdf 2.7节描述
+	 * */
 	public void destroy() {
+		//系统关闭之后做的清理工作,如果需要实现，可以实现相应的回调函数
 		jfinalConfig.beforeJFinalStop();
+		//停止相应的插件，关闭相应的资源，plugin一般都要实现stop方法，释放资源
 		jfinal.stopPlugins();
 	}
-	
+
+	/**
+	 * <p>通过传递的配置文件的位置，a_little_config.txt的例子</p>
+	 * example：<pre>
+	 *      <filter>
+				 <filter-name>jfinal</filter-name>
+				 <filter-class>com.jfinal.core.JFinalFilter</filter-class>
+				 <init-param>
+					 <param-name>configClass</param-name>
+					 <param-value>com.demo.common.DemoConfig</param-value>
+				 </init-param>
+			 </filter>
+	 * </pre>
+	 * */
 	private void createJFinalConfig(String configClass) {
 		if (configClass == null)
 			throw new RuntimeException("Please set configClass parameter of JFinalFilter in web.xml");
 		
 		Object temp = null;
 		try {
+			//通过反射创建配置文件对应的类，比如DemoConfig
 			temp = Class.forName(configClass).newInstance();
 		} catch (Exception e) {
 			throw new RuntimeException("Can not create instance of class: " + configClass, e);
